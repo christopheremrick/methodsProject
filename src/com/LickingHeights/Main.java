92bee7cd6788361d051c7e290d251da2e7d7e646
Chris <christopher.emrick.2022@student.lhschools.org>
2019-03-11 13:37:47 -0400

    Changed input to inputName

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
+        String inputName = scan.nextLine();
 
 
         helloWorld();
