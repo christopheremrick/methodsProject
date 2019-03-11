d27f1b3315677a2ef7e86de5c48133eaa6610f53
Chris <christopher.emrick.2022@student.lhschools.org>
2019-03-11 13:37:25 -0400

    Created Hello World Method
    Also made scanner class and created string called input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,21 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+
+
+        helloWorld();
+    }
+
+    public static void helloWorld() {
+
+        System.out.println("Hello World!");
+
     }
 }
